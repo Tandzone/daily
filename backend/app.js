03377bdc89962152e9a8040edc5e53033eb8349d
Tanjona <tanjonatsiry@gmail.com>
2025-06-19 12:52:47 +0300

    install cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const notesRouter = require('./routes/notesRoutes');
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 
 app.use(express.json());
 
